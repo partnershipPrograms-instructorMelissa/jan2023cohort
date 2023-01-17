e26a444a80df3753ee14c628d511e01cf496a233
Kaitlynn Whitney <kailiwhitney@hotmail.com>
2023-01-17 14:45:57 -0600

    Added arraysPredict question - not yet solved

---
+++ assignments/programmingBasics/week1/arraysPredict/main.js
@@ -0,0 +1,31 @@
+// == Predict 1 ==
+// var arr = [8,6,7,5,3,0,9]
+// for(var i = 0; i < arr.length; i++){    
+//     console.log(arr[i]);
+// }
+
+
+// == Predict 2 ==
+// var arr = [8,6,7,5,3,0,9]
+// for(var i = 0; i < arr.length; i++){    
+//     console.log(arr[i]);
+// }
+
+
+// == Predict 3 ==
+// var arr = [1,3,8,-5,0,-2,4,-1];
+// var newArr = [];
+// for(var i = 0; i< arr.length; i++){
+//     if(arr[i] < 0){
+//         newArr.push(arr[i]);
+//         arr[i] = arr[i] * -1;
+//     }
+//     else if(arr[i] == 0){
+//         arr[i] = "Zero";
+//     }
+//     else{
+//         arr[i] = arr[i] * -1;
+//     }
+// }
+// console.log(arr);
+// console.log(newArr);
