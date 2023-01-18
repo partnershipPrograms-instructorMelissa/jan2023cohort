1eba07befee7400990d19bdcb32cd95b172b303c
Joslyn <joslynsipod@gmail.com>
2023-01-18 16:07:20 -0600

    Arrays assignment done

---
+++ assignments/programmingBasics/ArraysPredict.js
@@ -0,0 +1,40 @@
+
+var arr = [8,6,7,5,3,0,9]
+for(var i = 0; i < arr.length; i++){    
+    console.log(arr[i]);
+}
+
+//I predict it will print the list of numbers in the array code
+
+var arr = [7,3,8,4,2,0,1];
+for(var i = 0; i < arr.length; i++){ 
+    if(arr[i] % 2 == 0){
+        console.log(arr[i]);
+    } 
+    else{
+        console.log("That is odd!");
+    }
+}
+
+//I predict for this code tat for every odd number in the array it will print "That is odd!" and every even number will be printed as so. 
+
+var arr = [1,3,8,-5,0,-2,4,-1];
+var newArr = [];
+for(var i = 0; i< arr.length; i++){
+    if(arr[i] < 0){
+        newArr.push(arr[i]);
+        arr[i] = arr[i] * -1;
+    }
+    else if(arr[i] == 0){
+        arr[i] = "Zero";
+    }
+    else{
+        arr[i] = arr[i] * -1;
+    }
+}
+console.log(arr);
+console.log(newArr);
+
+
+// The newArr will output the negative numbers in the original arr. For arr it will out put the opposite integer of original array such as [-1, -3, -8, 5, 0, 2, -4, 1].
+
