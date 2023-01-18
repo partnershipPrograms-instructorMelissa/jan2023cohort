dad06adb15a05990743845cfc5f9ac99572fd20f
Kaitlynn Whitney <kailiwhitney@hotmail.com>
2023-01-18 16:33:24 -0600

    Added playingWithLoops assignment - not yet complete

---
+++ assignments/java/scripts.js
@@ -0,0 +1,24 @@
+// == Programing Languages and Frameworks ==
+let tools = [
+    "HTML",
+    "JavaScript",
+    "CSS",
+    "C#",
+    "Java",
+    "Python",
+    "Spring Boot",
+    ".NET",
+    "PHP",
+    "Google Fu",
+    "Git",
+    "Github"
+] // hard coded tools
+
+console.log(tools)
+for(let i = 0; i < tools.length; i++){
+    console.log("looping through array",tools[i])
+    //return tools[i] -> needs to be in function
+}
+
+
+// == Clock ==
---
+++ assignments/programmingBasics/week1/playingWithObjects/main.js
@@ -0,0 +1,13 @@
+// given an array of objects
+var users = [{name: "Michael", age:37}, {name: "John", age:30}, {name: "David", age:27}];
+
+// print/log John's age
+
+// print/log name of the first object
+
+// print/log name and age of each user, using a for loop
+
+//OUTPUT
+// Michael - 37
+// John - 30
+// David - 27
