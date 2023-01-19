5311fe962dd450d50391d1ff8194e576fadd5c7b
Emily Vazquez <emily.m.vazquez99@gmail.com>
2023-01-19 15:39:51 -0600

    set up new folders and files for assignments

--- assignments/programmingBasics/javascript/functions/functions-predict.js
+++ assignments/programmingBasics/javascript/functions/functions-predict.js
@@ -1,1 +1,35 @@
-// functions
+//! PREDICTIONS
+
+//* Predict 1
+function greeting() {
+  return "Hello";
+  console.log("World");
+}
+var word = greeting();
+console.log(word);
+
+
+//* Predict 2
+function add(num1, num2) {
+  console.log("Summing Numbers!");
+  console.log("num1 is: " + num1);
+  console.log("num2 is: " + num2);
+  var sum = num1 + num2;
+  return sum;
+}
+var result1 = add(3, 5);
+var result2 = add(4, 7);
+console.log(result1);
+console.log(result2);
+
+
+//* Predict 3
+function highFive(num) {
+  for (var i = 0; i < num; i++) {
+    if (i == 5) {
+      console.log("High Five!");
+    } else {
+      console.log(i);
+    }
+  }
+}
