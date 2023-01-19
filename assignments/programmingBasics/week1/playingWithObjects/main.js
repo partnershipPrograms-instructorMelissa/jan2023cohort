13133960eb602fe170a59f3590ede715b078ad99
Kaitlynn Whitney <kailiwhitney@hotmail.com>
2023-01-19 13:03:04 -0600

    playingWithObjects assignment complete

--- assignments/programmingBasics/week1/playingWithObjects/main.js
+++ assignments/programmingBasics/week1/playingWithObjects/main.js
@@ -1,13 +1,18 @@
 // given an array of objects
-var users = [{name: "Michael", age:37}, {name: "John", age:30}, {name: "David", age:27}];
+let users = [{name: "Michael", age:37}, {name: "John", age:30}, {name: "David", age:27}]
 
 // print/log John's age
+// users[1].age = 31    // log
+// console.log(users[1].age)   // print
 
 // print/log name of the first object
+// users[0].name = 'Bob'    // log
+// console.log(users[0].name)  // print
 
 // print/log name and age of each user, using a for loop
-
-//OUTPUT
+for(let i = 0; i < users.length; i++)
+    console.log(`${users[i].name} - ${users[i].age}`)
+//OUTPUT:
 // Michael - 37
 // John - 30
 // David - 27
