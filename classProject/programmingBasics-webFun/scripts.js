2c4915604c73f9d58444b216011d167db10c9577
Melissa Longenberger <mlongenberger@codingdojo.com>
2023-01-20 16:27:14 -0500

    saving afternoon

--- classProject/programmingBasics-webFun/scripts.js
+++ classProject/programmingBasics-webFun/scripts.js
@@ -14,12 +14,16 @@ let tools = [
     "Git",
     "Github"
 ]
-console.log("The root Array:", tools)
-for(let i = 0; i < tools.length; i++){
-    console.log("looping through array:", tools[i])
-    // return tools[i]
+// console.log("The root Array:", tools)
+// for(let i = 0; i < tools.length; i++){
+//     console.log("looping through array:", tools[i])
+//     // return tools[i]
+// }
+function siteTools(arr) {
+    for(let i = 0; i  < arr.length; i++) {
+        console.log(arr[i])
+    }
 }
-
 
 
 
---
+++ lectures/programmingBasics/week1/day4/practice.js
@@ -0,0 +1,20 @@
+var arr01 = ["Bear", "Abby", "Lucy", "Roxy","Copper"]
+var arr02 = [1,2,3]
+var arr03 = [-1, 3, 6,-6, 9, -24, 8, 6, -100, 5000]
+var arr04 = [2,5,7,3,4,6,9]
+
+// Create a function that can use any of the above arrays and prints each value
+
+
+
+
+// Given 2 numbers create a function that adds, subtracts, multiplies, and divides them and pushes each result into an array.  Print the array of results
+
+
+// Create a function that takes any negative numbers and puts them in a new array and replaces that negative number with the word "negative", print both arrays
+
+
+// Create a function that replaces any even number with the word "even"
+
+
+
