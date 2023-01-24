daa8d525ac14133453d817c30ea79d07cd7c43f7
mligon115 <mligon115@yahoo.com>
2023-01-24 16:37:53 -0600

    completed assignment

--- assignments/programmingBasics/Week2/jan24Intermediate.js
+++ assignments/programmingBasics/Week2/jan24Intermediate.js
@@ -89,3 +89,12 @@ function secLargest(arr){
 //secLargest([-42, -1, -4, -3.14, -7]);
 
 //Double Trouble: Create a function that changes a given array to list each existing element twice, retaining original order.  Convert [4, "Ulysses", 42, false] to [4,4, "Ulysses", "Ulysses", 42, 42, false, false].
+function doubleTrouble(arr){
+    let res = []
+
+    for(let i = 0; i < arr.length; i++){
+        res.push(arr[i],arr[i])
+    }
+    console.log(res)
+}
+//doubleTrouble([4, 'Ulysses', 42, false]);
