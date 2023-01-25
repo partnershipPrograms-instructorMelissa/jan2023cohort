847674fd2d5f17cbde58fc5bb52782f43b5337f3
Emily Vazquez <emily.m.vazquez99@gmail.com>
2023-01-25 09:01:11 -0600

    working on foundations

--- assignments/programmingBasics/javascript/functions/basic-found2.js
+++ assignments/programmingBasics/javascript/functions/basic-found2.js
@@ -1,29 +1,120 @@
-//! Basic Foundation 2 algo challenge 
+//! Basic Foundation 2 algo challenge
 
 //* 1. Biggie Size
 // Given an array, write a function that changes all positive numbers in the array to the string "big".  Example: makeItBig([-1,3,5,-5]) returns that same array, changed to [-1, "big", "big", -5].
-
+function makeItBig(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > 0) {
+      arr[i] = "big";
+    }
+    return arr;
+    // console.log(arr);
+  }
+}
+// makeItBig([-1, 3, 5, -5]);
+// works but prints array each time??
 
 //* 2. Print Low, Return High
 // Create a function that takes in an array of numbers.  The function should print the lowest value in the array, and return the highest value in the array.
+function printLowReturnHigh(arr) {
+  var low = arr[0];
+  var high = arr[0];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < low) {
+      low = arr[i];
+    }
+    if (arr[i] > high) {
+      high = arr[i];
+    }
+  }
+  console.log(low);
+  return high;
+}
+// printLowestHighest([0, 1, 2, 3, 4, 5, 6]);
 
 //* 3. Print One, Return Another
 //Build a function that takes in an array of numbers.  The function should print the second-to-last value in the array, and return the first odd value in the array.
+function printOneReturnOne(arr) {
+  console.log(arr[arr.length - 2]);
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 !== 0) return arr[i];
+    // console.log(arr[i]);
+  }
+}
+// // printOneReturnOne([0, 1, 2, 3, 4, 5, 6]);
+// var a = [0, 1, 2, 3, 4, 5, 6]
+// console.log(printOneReturnOne(a));
 
 //* 4. Double Vision
 //Given an array (similar to saying 'takes in an array'), create a function that returns a new array where each value in the original array has been doubled.  Calling double([1,2,3]) should return [2,4,6] without changing the original array.
+function double(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    arr[i] = arr[i] * 2;
+  }
+  return arr;
+}
+// var b = [0, 1, 2, 3, 4];
+// console.log(double(b));
 
 //* 5. Count Positives
 //Given an array of numbers, create a function to replace the last value with the number of positive values found in the array.  Example, countPositives([-1,1,1,1]) changes the original array to [-1,1,1,3] and returns it.
+function countPositives(arr) {
+  var sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > 0) sum++;
+  }
+  arr[arr.length - 1] = sum;
+  return arr;
+}
+
+// var c = [-1,1,1,1]
+// console.log(countPositives(c));
 
 //* 6. Evens and Odds
 //Create a function that accepts an array.  Every time that array has three odd values in a row, print "That's odd!".  Every time the array has three evens in a row, print "Even more so!".
+function evenAndOdd(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 !== 0) {
+      if (arr[i + 1] % 2 !== 0) {
+        if (arr[i + 2] % 2 !== 0) {
+          console.log(`That's odd!`);
+        }
+      }
+    }
+
+    if (arr[i] % 2 == 0) {
+      if (arr[i + 1] % 2 == 0) {
+        if (arr[i + 2] % 2 == 0) {
+          console.log(`Even more so!`);
+        }
+      }
+    }
+  }
+}
+
+// var d = [2 ,4 ,6 ,1 ,5 ,7, 8, 0, 6];
+// evenAndOdd(d);
 
 //* 7. Increment the Seconds
 //Given an array of numbers arr, add 1 to every other element, specifically those whose index is odd (arr[1], arr[3], arr[5], etc).  Afterward, console.log each array value and return arr.
+function incSec(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    if(i % 2 !== 0) {
+      arr[i] = arr[i] + 1;
+    }
+    console.log(arr[i]);
+  }
+  return arr;
+}
+
+// var e = [0, 1, 2, 3, 4, 5, 6]
+// console.log(incSec(e));
 
 //* 8. Previous Lengths
 // You are passed an array (similar to saying 'takes in an array' or 'given an array') containing strings.  Working within that same array, replace each string with a number - the length of the string at the previous array index - and return the array.  For example, previousLengths(["hello", "dojo", "awesome"]) should return ["hello", 5, 4]. Hint: Can for loops only go forward?
+function name(params) {
+  
+}
 
 //* 9. Add Seven to Most
 //Build a function that accepts an array. Return a new array with all the values of the original, but add 7 to each. Do not alter the original array.  Example, addSeven([1,2,3]) should return [8,9,10] in a new array.
