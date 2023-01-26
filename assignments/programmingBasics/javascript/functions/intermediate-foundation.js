2f33855e44c6f382fb3f6ce81f8e63f3e727ade2
Emily Vazquez <emily.m.vazquez99@gmail.com>
2023-01-26 09:49:07 -0600

    set up foundations practice page 2

---
+++ assignments/programmingBasics/javascript/functions/intermediate-foundation.js
@@ -0,0 +1,22 @@
+//! Intermediate FoundationS 
+
+//* Sigma - 
+// Implement function sigma(num) that, given a number, returns the sum of all positive integers up to the given number (inclusive).  Ex: sigma(3) = 6 (or 1+2+3); sigma(5) = 15 (or 1+2+3+4+5).
+
+//* Factorial - 
+// Write a function factorial(num) that, given a number, returns the product (multiplication) of all positive integers from 1 up to the given number (inclusive).  For example, factorial(3) = 6 (or 1*2*3); factorial(5) = 120 (or 1*2*3*4*5).
+
+//* Fibonacci -
+// Create a function to generate Fibonacci numbers.  In this famous mathematical sequence, each number is the sum of the previous two, starting with values 0 and 1.  Your function should accept one argument, an index into the sequence (where 0 corresponds to the initial value, 4 corresponds to the value four later, etc).  Examples: fibonacci(0) = 0 (given), fibonacci(1) = 1 (given), fibonacci(2) = 1 (fib(0)+fib(1), or 0+1), fibonacci(3) = 2 (fib(1) + fib(2)3, or 1+1), fibonacci(4) = 3 (1+2), fibonacci(5) = 5 (2+3), fibonacci(6) = 8 (3+5), fibonacci(7) = 13 (5+8).  
+
+//* Array: Second-to-Last:
+//  Return the second-to-last element of an array. Given [42, true, 4, "Liam", 7], return "Liam".  If array is too short, return null.
+
+//* Array: Nth-to-Last: 
+// Return the element that is N-from-array's-end.  Given ([5,2,3,6,4,9,7],3), return 4.  If the array is too short, return null.
+
+//* Array: Second-Largest:
+//  Return the second-largest element of an array. Given [42,1,4,3.14,7], return 7.  If the array is too short, return null.
+
+//* Double Trouble:
+//  Create a function that changes a given array to list each existing element twice, retaining original order.  Convert [4, "Ulysses", 42, false] to [4,4, "Ulysses", "Ulysses", 42, 42, false, false].
