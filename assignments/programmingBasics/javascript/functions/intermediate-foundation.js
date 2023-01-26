8d77781bbebda9ed00a68bd8d40f2fde317bb89d
Emily Vazquez <emily.m.vazquez99@gmail.com>
2023-01-26 09:49:45 -0600

    minor changes

--- assignments/programmingBasics/javascript/functions/intermediate-foundation.js
+++ assignments/programmingBasics/javascript/functions/intermediate-foundation.js
@@ -2,6 +2,10 @@
 
 //* Sigma - 
 // Implement function sigma(num) that, given a number, returns the sum of all positive integers up to the given number (inclusive).  Ex: sigma(3) = 6 (or 1+2+3); sigma(5) = 15 (or 1+2+3+4+5).
+function sigma(num) {
+  
+}
+
 
 //* Factorial - 
 // Write a function factorial(num) that, given a number, returns the product (multiplication) of all positive integers from 1 up to the given number (inclusive).  For example, factorial(3) = 6 (or 1*2*3); factorial(5) = 120 (or 1*2*3*4*5).
