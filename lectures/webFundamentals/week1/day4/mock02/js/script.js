5e65d0d9b18e4b02b7e9f69a439a06e49afd2215
Melissa Longenberger <mlongenberger@codingdojo.com>
2023-02-02 19:00:20 -0500

    pushing last changes for the day

---
+++ lectures/webFundamentals/week1/day4/mock02/js/script.js
@@ -0,0 +1,13 @@
+console.log('wired up')
+
+function hideMe() {
+    document.getElementById('hide').style.display = 'none'
+}
+
+function alertMe(element) {
+    // console.log('clicked city', element)
+    alert('Loading weather report...')
+}
+
+// change temps to f on change of select and back c if selected
+// 24-75, 
