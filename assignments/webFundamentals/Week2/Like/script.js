411c4419403ac5415000c0083cb3ec2e8784da4b
mligon115 <mligon115@yahoo.com>
2023-02-07 17:28:25 -0600

    completed homework

--- assignments/webFundamentals/Week2/Like/script.js
+++ assignments/webFundamentals/Week2/Like/script.js
@@ -1,9 +1,23 @@
 console.log('connected!')
 
 let count = 0;
+let count1 = 0;
+let count2 = 0;
+let val = []
 
 function addLikes(element) {
-    count+=1;
-    document.getElementById('num').innerText = count
-    console.log(count)
+    count += 1;
+    document.getElementById('num').innerText = count;
+ }
+
+function addLikes1(element) {
+  count1 += 1;
+  document.getElementById('num1').innerText = count1;
+  console.log(count);
+}
+
+function addLikes2(element) {
+  count2 += 1;
+  document.getElementById('num2').innerText = count2;
+  console.log(count);
 }
