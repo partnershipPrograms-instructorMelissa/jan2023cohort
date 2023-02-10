ca0be14534e8e53d9f9b57e1c3bf1960b8a000aa
Joslyn <joslynsipod@gmail.com>
2023-02-10 11:31:56 -0600

    finished

---
+++ assignments/webFundamentals/ButtonClicker/button.js
@@ -0,0 +1,11 @@
+function changeLogin(element) {
+    element.innerText = "Logout";
+}
+
+function hide(element) {
+    element.remove();
+}
+
+// function alert(element) {
+//     alert("ninja liked this");
+// }
