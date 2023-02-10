0cb340c35db5a094c89b795505cef9ae97ce2770
mligon115 <mligon115@yahoo.com>
2023-02-10 14:45:53 -0600

    saving

---
+++ assignments/cSharp/Fundamentals1/Program.cs
@@ -0,0 +1,13 @@
+﻿for(int i = 1; i <= 255; i++){
+    // Console.WriteLine(i);
+}
+
+Random rand = new Random();
+for(int i = 1; i <= 5; i++) {
+    // Console.WriteLine(rand.Next(10,21));
+}
+
+for (int i = 1; i <= 5; i++) {   
+    i += rand.Next(10, 21);
+    // Console.WriteLine(i); 
+}
