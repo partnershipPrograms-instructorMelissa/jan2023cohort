7d3e93a3ffb49754b67b630c7c5173312fa6e3e5
mligon115 <mligon115@yahoo.com>
2023-02-11 17:24:09 -0600

    saving

--- assignments/cSharp/Fundamentals2/Program.cs
+++ assignments/cSharp/Fundamentals2/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Create an integer array with the values 0 through 9 inside.
+int[] newArray = new int[] {0,1,2,3,4,5,6,7,8,9};
+
+//Create a string array with the names "Tim", "Martin", "Nikki", and "Sara".
+string[] newStringArray = new string[] {"Tim", "Martin", "Nikki", "Sara" };
+
+//Create a boolean array of length 10. Then fill it with alternating true/false values, starting with true. (Tip: do this using logic! Do not hard-code the values in!)
+bool[] newBoolArray = new bool[10];
+
+for (int i = 0; i < newBoolArray.Length; i++)
+{
+    if(i % 2 == 0){
+        newBoolArray[i] = true;
+    }else{
+        newBoolArray[i]= false;
+    }
+    Console.WriteLine(newBoolArray[i]);
+}
+
+
+// for(int i = 0; i < newStringArray.Length; i++)
+// {
+//     Console.WriteLine(newStringArray[i]);
+// }
