0fcb266bbc7172ce0693b7620142009d52e80c28
mligon115 <mligon115@yahoo.com>
2023-02-11 18:07:59 -0600

    saving

--- assignments/cSharp/Fundamentals2/Program.cs
+++ assignments/cSharp/Fundamentals2/Program.cs
@@ -2,7 +2,7 @@
 int[] newArray = new int[] {0,1,2,3,4,5,6,7,8,9};
 
 //Create a string array with the names "Tim", "Martin", "Nikki", and "Sara".
-string[] newStringArray = new string[] {"Tim", "Martin", "Nikki", "Sara" };
+string[] nameArray = new string[] {"Tim", "Martin", "Nikki", "Sara" };
 
 //Create a boolean array of length 10. Then fill it with alternating true/false values, starting with true. (Tip: do this using logic! Do not hard-code the values in!)
 bool[] newBoolArray = new bool[10];
@@ -33,14 +33,26 @@ myIceCream.RemoveAt(2);
 Console.WriteLine(myIceCream.Count);
 
 //Create a dictionary that will store string keys and string values.
-var myString = new Dictionary<string, string>
+// var myString = new Dictionary<string, string>
+// {
+//     {"Tim", "Vanilla"},
+//     {"Martin", "Chocalate"},
+//     {"Nikki", "Butter Pecan"},
+//     {"Sara", "Chocolate Chip"}
+// };
+// foreach(KeyValuePair<string, string> entry in myString)
+// {
+//     Console.WriteLine($"{entry.Key} - {entry.Value}");
+// }
+Dictionary<string, string> newDict = new Dictionary<string, string>();
+
+Random rand = new Random();
+for(int i = 0; i < nameArray.Length; i++)
 {
-    {"Tim", "Vanilla"},
-    {"Martin", "Chocalate"},
-    {"Nikki", "Butter Pecan"},
-    {"Sara", "Chocolate Chip"}
-};
-foreach(KeyValuePair<string, string> entry in myString)
+    newDict.Add(nameArray[i], myIceCream[rand.Next(0,myIceCream.Count)]);
+}
+
+foreach(KeyValuePair<string, string> entry in newDict)
 {
     Console.WriteLine($"{entry.Key} - {entry.Value}");
 }
