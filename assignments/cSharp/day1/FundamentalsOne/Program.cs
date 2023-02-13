54ee19652660a8d34c51da9ab24cb629a64d5630
Emily Vazquez <emily.m.vazquez99@gmail.com>
2023-02-13 15:53:37 -0600

    started c# stuff began fundamental assignments

---
+++ assignments/cSharp/day1/FundamentalsOne/Program.cs
@@ -0,0 +1,46 @@
+﻿
+//* Create a loop that prints all values from 1-255.
+for (int i = 1; i <= 255; i++)
+{
+  // Console.WriteLine(i);
+}
+
+
+//* Create a new loop that generates 5 random numbers between 10 and 20.
+// Random rand = new Random();
+for (int i = 1; i <= 5; i++)
+{
+  // Console.WriteLine(rand.Next(10, 21));
+}
+
+
+//* Modify the previous loop to add the random values together and print the sum after the loop finishes.
+// Random rand = new Random();
+// int random = 0;
+// int line = 0;
+// int sum = 0;
+// for (int i = 1; i <= 5; i++)
+// {
+//   line++;
+//   random = rand.Next(10, 21);
+//   sum += random;
+//   Console.WriteLine("number: {0}: {1}", line, random);
+
+// }
+// Console.WriteLine("Sum is " + sum);
+
+
+//* Create a new loop that prints all values from 1 to 100 that are divisible by 3 OR 5, but NOT both. 
+// int line = 0;
+// int sum = 0;
+for (int i = 1; i <= 100; i++)
+{
+  if (i % 3 == 0 || i % 5 == 0) ;
+  {
+    if (i % 3 == 0 && i % 5 == 0) ;
+
+    Console.WriteLine(i);
+  }
+  Console.WriteLine(i);
+}
+
---
+++ assignments/cSharp/day1/FundamentalsTwo/Program.cs
@@ -0,0 +1,16 @@
+﻿//* Three Basic Arrays
+// Create an integer array with the values 0 through 9 inside.
+
+// Declaring an array of length 5, initialized by default to all zeroes
+int[] numArray = new int[9];
+Console.WriteLine(numArray);
+// Declaring an array with pre-populated values
+// For arrays initialized this way, the length is determined by the size of the given data
+// int[] numArray2 = {1,2,3,4,6};
+
+
+
+
+
+// Create a string array with the names "Tim", "Martin", "Nikki", and "Sara".
+// Create a boolean array of length 10. Then fill it with alternating true/false values, starting with true. (Tip: do this using logic! Do not hard-code the values in!)
