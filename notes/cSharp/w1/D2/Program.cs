7753f370b32796c8d2974e6ce35d75e998468b8c
Kaitlynn Whitney <kailiwhitney@hotmail.com>
2023-02-14 14:08:01 -0600

    Added w1d2 notes

---
+++ notes/cSharp/w1/D2/Program.cs
@@ -0,0 +1,14 @@
+﻿//  == functions ==
+//  static
+//      - static function can be called within the scope of its definition or by calling it on the class it's defined in
+//      - non-static method has to be called on an instance of an object
+static void SayHello(){ // w/o parameters
+    Console.WriteLine($"Hello!");
+}
+
+static int AddNumbers(int a, int b) // w/ parameters
+{
+    return a + b;
+}
+
+SayHello(); // call function
