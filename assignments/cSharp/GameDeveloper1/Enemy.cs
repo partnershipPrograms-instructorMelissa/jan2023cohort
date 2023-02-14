ec078396b60306a07cf00ed653e55f84bf7e7de2
mligon115 <mligon115@yahoo.com>
2023-02-13 18:16:50 -0600

    testing the code

--- assignments/cSharp/GameDeveloper1/Enemy.cs
+++ assignments/cSharp/GameDeveloper1/Enemy.cs
@@ -1,4 +1,13 @@
 class Enemy 
 {
-    
+    public string Name;
+    public int Health;
+    public List<string> AttackList {get; set;}
+
+    public Enemy(string name, int health, List<string> attackList)
+    {
+        Name = name;
+        Health = 100;
+        AttackList = new List<string>();
+    }
 }
