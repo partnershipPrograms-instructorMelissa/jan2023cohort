0c76db583c1f6ff9a4c2902e311c97e8a0770ab4
mligon115 <mligon115@yahoo.com>
2023-02-13 18:35:01 -0600

    saving

--- assignments/cSharp/GameDeveloper1/Program.cs
+++ assignments/cSharp/GameDeveloper1/Program.cs
@@ -1,5 +1,7 @@
 ﻿Enemy Player1 = new Enemy("Bandit");
 Player1.AttackList.Add("fireball");
+Player1.AttackList.Add("punch");
+Player1.AttackList.Add("throw");
 // Console.WriteLine(Player1.Health);
 
 foreach(String Attack in Player1.AttackList)
