592a21066960a14da37778fba1de0350f7d8ac2a
Emily Vazquez <emily.m.vazquez99@gmail.com>
2023-02-15 08:45:19 -0600

    working on some c# stuff

---
+++ assignments/cSharp/day2/FundamentalsThree/Program.cs
@@ -0,0 +1,10 @@
+﻿// // See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+
+bool[] boolean1 = new bool[10];
+for (int i = 0; i < boolean1.Length; i += 2)
+{
+  boolean1[i] = true;
+  Console.WriteLine(boolean1);
+}
