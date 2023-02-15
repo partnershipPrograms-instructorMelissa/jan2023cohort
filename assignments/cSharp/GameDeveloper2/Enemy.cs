266389ac490d3597d9fd5a339e09834a46147658
mligon115 <mligon115@yahoo.com>
2023-02-15 10:56:46 -0600

    saving

---
+++ assignments/cSharp/GameDeveloper2/Enemy.cs
@@ -0,0 +1,40 @@
+class Enemy
+{
+    public string EnemyName;
+    private int Health;
+
+    public List<string> AttackList { get; set; }
+
+    public int _Health
+    {
+        get
+        {
+            return Health;
+        }
+    }
+
+    public void RandomAttack()
+    {
+        Random rnd = new Random();
+
+        List<string> attackChoices = AttackList;
+
+        int fIndex = rnd.Next(attackChoices.Count);
+
+        Console.WriteLine($"Your Attack is: {attackChoices[fIndex]}");
+    }
+    // public void PerformAttack(Enemy Target, Attack DamageAmount)
+    // {
+    //     string target = EnemyName;
+    //     string chosenAttack = AttackName;
+
+    // }
+
+    public Enemy(string enemyName)
+    {
+        EnemyName = enemyName;
+        Health = 100;
+        AttackList = new List<string>();
+    }
+
+}
