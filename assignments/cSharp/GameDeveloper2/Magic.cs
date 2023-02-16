379a25ca9ac768351ee53578121dd5d44c1c978e
mligon115 <mligon115@yahoo.com>
2023-02-16 16:42:15 -0600

    saving

--- assignments/cSharp/GameDeveloper2/Magic.cs
+++ assignments/cSharp/GameDeveloper2/Magic.cs
@@ -3,10 +3,10 @@ class Magic : Enemy
     public void HealMethod(Enemy Target)
     {
         int targetEnemy = Target.Health;
-        
+             
+        targetEnemy += 40;
 
-
-
+        Console.WriteLine($"your new health total is {targetEnemy}");
     }
     public Magic(string enemyName, string attackName, int damageAmount) : base(enemyName, attackName, damageAmount)
     {
