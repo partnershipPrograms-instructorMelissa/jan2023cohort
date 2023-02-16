73ee416f614a43d73659d7c1e8f71b6618ad7fcf
Emily Vazquez <emily.m.vazquez99@gmail.com>
2023-02-15 23:38:27 -0600

    completed c# fundamentals 2

--- assignments/cSharp/day1/FundamentalsTwo/Program.cs
+++ assignments/cSharp/day1/FundamentalsTwo/Program.cs
@@ -1,16 +1,84 @@
 ﻿//* Three Basic Arrays
 // Create an integer array with the values 0 through 9 inside.
+int[] intArray = new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+// foreach (var num in intArray)
+//   Console.WriteLine(num.ToString());
 
-// Declaring an array of length 5, initialized by default to all zeroes
-int[] numArray = new int[9];
-Console.WriteLine(numArray);
-// Declaring an array with pre-populated values
-// For arrays initialized this way, the length is determined by the size of the given data
-// int[] numArray2 = {1,2,3,4,6};
-
-
-
-
-
+//*------------------------------------------------------------------------------ 
 // Create a string array with the names "Tim", "Martin", "Nikki", and "Sara".
+// string[] names = new string[4];
+// names[0] = "Tim";
+// names[1] = "Martin";
+// names[2] = "Nikki";
+// names[3] = "Sara";
+// foreach (string name in names)
+// Console.WriteLine(name);
+
+string[] names = new string[] { "Tim", "Martin", "Nikki", "Sara" };
+// foreach (string name in names)
+// Console.WriteLine(name.ToString());
+//*------------------------------------------------------------------------------ 
 // Create a boolean array of length 10. Then fill it with alternating true/false values, starting with true. (Tip: do this using logic! Do not hard-code the values in!)
+bool[] array = new bool[10];
+for (int i = 0; i < array.Length; i++)
+{
+  if (i % 2 == 0)
+  {
+    array[i] = true;
+  }
+  // Console.WriteLine(array[i]);
+}
+
+//*----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+//* List of Flavors
+
+// Create a string List of ice cream flavors that holds at least 5 different flavors. (Feel free to add more than 5!)
+// Output the length of the List after you added the flavors.
+// Output the third flavor in the List.
+// Now remove the third flavor using its index location.
+// Output the length of the List again. It should now be one fewer.
+
+List<string> iceCream = new List<string>();
+iceCream.Add("Chocolate");
+iceCream.Add("Vanilla");
+iceCream.Add("Strawberry");
+iceCream.Add("Mint Chocolate Chip");
+iceCream.Add("Rocky Road");
+iceCream.Add("Neapolitan");
+iceCream.Add("Cookies and Cream");
+iceCream.Add("Butter Pecan");
+
+// Console.WriteLine(iceCream.Count);
+// Console.WriteLine(iceCream[2]);
+// iceCream.RemoveAt(2);
+// Console.WriteLine(iceCream.Count);
+//*----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+//* User Dictionary
+
+// Create a dictionary that will store string keys and string values.
+// Add key/value pairs to the dictionary where:
+// Each key is a name from your names array (this can be done by hand or using logic)
+// Each value is a randomly selected flavor from your flavors List (remember Random from earlier?)
+// Loop through the dictionary and print out each user's name and their associated ice cream flavor.
+
+Dictionary<string, string> nameFlavor = new Dictionary<string, string>();
+Random rand = new Random();
+for (int i = 0; i < names.Length; i++)
+{
+  nameFlavor.Add(names[i], iceCream[rand.Next(iceCream.Count)]);
+}
+foreach (KeyValuePair<string, string> item in nameFlavor)
+{
+  Console.WriteLine($"{item.Key}'s flavor of choice is {item.Value}");
+}
+{
+
+}
+
+
+
+// Dictionary<string, int> namesList = new Dictionary<string, int>();
+// namesList.Add("Lizette", 23);
+// namesList.Add("Melissa", 44);
---
+++ notes/cSharp/ClassReview/Program.cs
@@ -0,0 +1,138 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine("------------------------------------------------------------------------------------------------------------------------------------------------------");
+
+//* 1. How do I print Welcome to class?
+Console.WriteLine("Welcome to class!");
+
+Console.WriteLine("------------------------------------------------------------------------------------------------------------------------------------------------------");
+
+//* 2. Print the numbers 1-10 
+int count = 1;
+while (count < 11)
+{
+  Console.WriteLine($"{count}");
+  count++;
+}
+Console.WriteLine("------------------------------------------------------------------------------------------------------------------------------------------------------");
+
+//* 3. Create a list with 4 animals in it
+List<string> animals = new List<string>();
+animals.Add("Zebra");
+animals.Add("Lion");
+animals.Add("Tiger");
+animals.Add("Bear");
+
+Console.WriteLine("------------------------------------------------------------------------------------------------------------------------------------------------------");
+
+//* 4. Create a empty array and add 4 foods to it
+string[] foods = new string[4];
+foods[0] = "Pizza";
+foods[1] = "Wings"; 
+foods[2] = "Sushi";
+foods[3] = "Ramen";
+
+Console.WriteLine("------------------------------------------------------------------------------------------------------------------------------------------------------");
+
+//* 5. Create a dictionary of names and ages
+Dictionary<string, int> namesList = new Dictionary<string, int>();
+namesList.Add("Lizette", 23);
+namesList.Add("Melissa", 44);
+
+Console.WriteLine("------------------------------------------------------------------------------------------------------------------------------------------------------");
+
+//* 6. Loop through the list of animals
+for (int i = 0; i < 4; i++)
+{
+  Console.WriteLine(animals[i]);
+}
+
+for (int i = 0; i < animals.Count; i++)
+{
+  Console.WriteLine(animals[i]);
+}
+
+Console.WriteLine("------------------------------------------------------------------------------------------------------------------------------------------------------");
+
+//* 7. Loop through the array of food
+foreach (string food in foods)
+{
+  Console.WriteLine(food);
+}
+
+Console.WriteLine("------------------------------------------------------------------------------------------------------------------------------------------------------");
+
+//* 8. Loop through the dictionary
+foreach (var entry in namesList)
+{
+  Console.WriteLine($"{entry.Key} is {entry.Value} years old");
+}
+
+Console.WriteLine("------------------------------------------------------------------------------------------------------------------------------------------------------");
+
+//* 9. Check if dictionary entries are a string or int
+foreach (var entry in namesList)
+{
+  int results = entry.Value;
+  // if(int.TryParse(results, out number)) { // didn't work as results was already a int and tryParse changes from string to int ("2" to 2)
+  if (results is int)
+  {
+    Console.WriteLine("This is a number");
+  }
+  else
+  {
+    Console.WriteLine("This is not a number");
+  }
+}
+
+Console.WriteLine("------------------------------------------------------------------------------------------------------------------------------------------------------");
+
+//* 10. Create a list of grades
+List<int> gradeList = new List<int>();
+gradeList.Add(99);
+gradeList.Add(80);
+gradeList.Add(95);
+
+Console.WriteLine("------------------------------------------------------------------------------------------------------------------------------------------------------");
+
+//* 11. Create a function that finds the average of the grades
+static double average(List<int> list)
+{
+  int sum = 0;
+  foreach (int grade in list)
+  {
+    sum += grade;
+  }
+  return (double)sum / list.Count; // The double here allows for the sum (declared as int and Count default of int to return as a double)
+}
+Console.WriteLine(average(gradeList));
+
+Console.WriteLine("------------------------------------------------------------------------------------------------------------------------------------------------------");
+
+//* 12. Pull a random name from the dictionary
+// dictionary<string, int> namesList = new dictionary<string, int>();
+Random random = new Random();
+int rNum = random.Next(namesList.Count); // creating a var rKey making it = a random number no larger than the number of idexes in the dictionary
+// string key = namesList.Keys.ElementAt(rKey); // pulling the 
+KeyValuePair<string, int> aKey = namesList.ElementAt(rNum); // KeyValuePair is a predefined class.  This is where we are pulling the random entry from the dictionary and assigning aKey to it
+Console.WriteLine("key: " + aKey.Key + ", value: " + aKey.Value);
+
+Console.WriteLine("------------------------------------------------------------------------------------------------------------------------------------------------------");
+
+//* 13. Print all the ages in the dictionary 
+// dictionary<string, int> namesList = new dictionary<string, int>();
+// namesList.Add("Lizette", 23);
+// namesList.Add("Melissa", 44);
+
+foreach (var age in namesList)
+{
+  Console.WriteLine("age: " + age.Value);
+}
+
+Console.WriteLine("------------------------------------------------------------------------------------------------------------------------------------------------------");
+
+//*
+string name = "Melissa";
+string altName = "Kaitlynn";
+Console.WriteLine(String.Format("Hello, {0}, my name is {1}", name, altName));
