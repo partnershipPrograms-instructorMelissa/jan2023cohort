c77ccc17684e5b1f96877b9d274ed964fb25d743
Kaitlynn Whitney <kailiwhitney@hotmail.com>
2023-02-16 12:53:52 -0600

    Game Development I assignment complete

---
+++ assignments/cSharp/GameDeveloper1/Program.cs
@@ -0,0 +1,9 @@
+﻿Enemy Ogre = new Enemy("Ogre");
+
+Attack ogreAttack = new Attack("Spit ball", 5);
+Ogre.AddAttack(ogreAttack);
+ogreAttack = new Attack("Throw Rock", 9);
+Ogre.AddAttack(ogreAttack);
+ogreAttack = new Attack("Stomp Ground", 7);
+
+Ogre.RandomAttack();
