e41ca7999c194fd37508c69ee086319aae22c917
Emily Vazquez <emily.m.vazquez99@gmail.com>
2023-02-17 11:51:27 -0600

    committing to pull

--- assignments/cSharp/day2/FundamentalsThree/Program.cs
+++ assignments/cSharp/day2/FundamentalsThree/Program.cs
@@ -1,6 +1,45 @@
 ﻿// // See https://aka.ms/new-console-template for more information
 // Console.WriteLine("Hello, World!");
 
+//* 1. Iterate and print values
+static void PrintList(List<string> MyList)
+{
+  foreach (var item in MyList)
+  {
+    Console.WriteLine(item);
+  }
+  for (int i = 0; i < MyList.Count; i++)
+  {
+    Console.WriteLine(MyList[i]);
+  }
+  MyList.ForEach((p) => Console.WriteLine(p));
+  Console.WriteLine(string.Join(", ", MyList));
+}
+List<string> TestStringList = new List<string>() { "Harry", "Steve", "Carla", "Jeanne" };
+PrintList(TestStringList);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 bool[] boolean1 = new bool[10];
 for (int i = 0; i < boolean1.Length; i += 2)
