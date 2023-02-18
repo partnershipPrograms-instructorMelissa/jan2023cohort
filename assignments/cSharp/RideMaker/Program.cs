e42906945e2390cc3757059bc8f870be37b0c085
mligon115 <mligon115@yahoo.com>
2023-02-17 19:15:23 -0600

    saving

--- assignments/cSharp/RideMaker/Program.cs
+++ assignments/cSharp/RideMaker/Program.cs
@@ -3,7 +3,7 @@ Vehicle nissan = new Vehicle("Nissan", "tan");
 Vehicle suburban = new Vehicle("Suburban", 4, "blue", true);
 Vehicle suburu = new Vehicle("Suburu", "red");
 
-List<string> allVehicles = new List<string>();
+List<Vehicle> allVehicles = new List<Vehicle>();
 
 allVehicles.Add(tesla);
 allVehicles.Add(nissan);
@@ -12,6 +12,5 @@ allVehicles.Add(suburu);
 
 foreach(Vehicle vehicle in allVehicles)
 {
-    Console.WriteLine(vehicle.ShowInfo());
-    
+    vehicle.ShowInfo();   
 }
