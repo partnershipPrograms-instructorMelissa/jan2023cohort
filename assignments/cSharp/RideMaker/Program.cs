5486641705bab2d1738348365ee72c8f62550f84
mligon115 <mligon115@yahoo.com>
2023-02-17 19:10:52 -0600

    saving

--- assignments/cSharp/RideMaker/Program.cs
+++ assignments/cSharp/RideMaker/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Vehicle tesla = new Vehicle("Tesla", 4, "white", false);
+Vehicle nissan = new Vehicle("Nissan", "tan");
+Vehicle suburban = new Vehicle("Suburban", 4, "blue", true);
+Vehicle suburu = new Vehicle("Suburu", "red");
+
+List<string> allVehicles = new List<string>();
+
+allVehicles.Add(tesla);
+allVehicles.Add(nissan);
+allVehicles.Add(suburban);
+allVehicles.Add(suburu);
+
+foreach(Vehicle vehicle in allVehicles)
+{
+    Console.WriteLine(vehicle.ShowInfo());
+    
+}
