8bee01d353f1b58f5ebbb2311520311f6792ce83
Emily Vazquez <emily.m.vazquez99@gmail.com>
2023-02-22 09:15:54 -0600

    fund 3 work

--- assignments/cSharp/day2/FundamentalsThree/Program.cs
+++ assignments/cSharp/day2/FundamentalsThree/Program.cs
@@ -2,48 +2,86 @@
 // Console.WriteLine("Hello, World!");
 
 //* 1. Iterate and print values
-static void PrintList(List<string> MyList)
+// static void PrintList(List<string> MyList)
+// {
+//   for (int i = 0; i < MyList.Count; i++)
+//   {
+//     Console.WriteLine(MyList[i]);
+//   }
+// }
+// List<string> TestStringList = new List<string>() { "Harry", "Steve", "Carla", "Jeanne" };
+// PrintList(TestStringList);
+
+
+//* 2. Print Sum
+// static void SumOfNumbers(List<int> IntList)
+// {
+//   int sum = 0;
+//   IntList.ForEach(n => sum += n);
+//   {
+//     Console.WriteLine($"The sum is: {sum}");
+//   }
+// }
+// List<int> TestIntList = new List<int>() { 2, 7, 12, 9, 3 };
+// // You should get back 33 in this example
+// SumOfNumbers(TestIntList);
+
+
+//* 3. Find Max
+// static int FindMax(List<int> IntList)
+// {
+//   int max = IntList[0];
+//   foreach (int n in IntList)
+//   {
+//     if (n > max)
+//     { max = n; }
+//   }
+//   return max;
+// }
+// List<int> TestIntList2 = new List<int>() { -9, 12, 10, 3, 17, 5 };
+// // You should get back 17 in this example
+// Console.WriteLine($"The max is {FindMax(TestIntList2)}");
+// FindMax(TestIntList2);
+
+
+//* 4. Square the Values
+static List<int> SquareValues(List<int> IntList)
 {
-  foreach (var item in MyList)
+  for (int i = 0; i < IntList.Count; i++)
   {
-    Console.WriteLine(item);
+    IntList[i] *= IntList[i];
   }
-  for (int i = 0; i < MyList.Count; i++)
-  {
-    Console.WriteLine(MyList[i]);
-  }
-  MyList.ForEach((p) => Console.WriteLine(p));
-  Console.WriteLine(string.Join(", ", MyList));
+  return IntList;
 }
-List<string> TestStringList = new List<string>() { "Harry", "Steve", "Carla", "Jeanne" };
-PrintList(TestStringList);
+List<int> TestIntList3 = new List<int>() { 1, 2, 3, 4, 5 };
+// You should get back [1,4,9,16,25], think about how you will show that this worked
+SquareValues(TestIntList3);
+TestIntList3.ForEach((n) => Console.WriteLine($"{TestIntList3[0]} squared is {n}"));
 
 
 
+//* 5. Replace Negative Numbers with 0
+// static int[] NonNegatives(int[] IntArray)
+// {
+//   for (int i = 0; i < IntArray.Length; i++)
+//   {
+//     if (IntArray[i] < 0)
+//     { IntArray[i] = 0; }
+//   }
+//   return IntArray;
+// }
+// int[] TestIntArray = new int[] { -1, 2, 3, -4, 5 };
+// // You should get back [0,2,3,0,5], think about how you will show that this worked
+// NonNegatives(TestIntArray);
+// foreach (int n in TestIntArray)
+// { Console.WriteLine(n); }
 
 
 
+//* 6. Print Dictionary
 
 
+//* 7. Find Key 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-bool[] boolean1 = new bool[10];
-for (int i = 0; i < boolean1.Length; i += 2)
-{
-  boolean1[i] = true;
-  Console.WriteLine(boolean1);
-}
+//* 8. Generate a Dictionary 
--- lectures/cSharp/week1/day3/ClassReview/Program.cs
+++ lectures/cSharp/week1/day3/ClassReview/Program.cs
@@ -1,33 +1,33 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-Console.WriteLine("------------------------------");
+//* Console.WriteLine("------------------------------");
 Console.WriteLine("Welcome to class!");
 
-Console.WriteLine("------------------------------");
+//* Console.WriteLine("------------------------------");
 int count = 1;
 while(count < 11) {
     Console.WriteLine($"{count}");
     count++;
 }
-Console.WriteLine("------------------------------");
+//* Console.WriteLine("------------------------------");
 List<string> animals = new List <string> ();
 animals.Add("Zebra");
 animals.Add("Lion");
 animals.Add("Tiger");
 animals.Add("Bear");
-Console.WriteLine("------------------------------");
+//* Console.WriteLine("------------------------------");
 
 string[] foods = new string[4];
 foods[0] = "Pizza";
 foods[1] = "Wings";
 foods[2] = "Sushi";
 foods[3] = "Ramen";
-Console.WriteLine("------------------------------");
+//* Console.WriteLine("------------------------------");
 
 Dictionary<string, int> namesList = new Dictionary<string, int>();
 namesList.Add("Lizette", 23);
 namesList.Add("Melissa", 44);
-Console.WriteLine("------------------------------");
+//* Console.WriteLine("------------------------------");
 
 for(int i = 0; i < 4; i++){
     Console.WriteLine(animals[i]);
@@ -36,15 +36,15 @@ for(int i = 0; i < 4; i++){
 for(int i = 0; i < animals.Count; i++){
     Console.WriteLine(animals[i]);
 }
-Console.WriteLine("------------------------------");
+//* Console.WriteLine("------------------------------");
 foreach(string food in foods) {
     Console.WriteLine(food);
 }
-Console.WriteLine("------------------------------");
+//* Console.WriteLine("------------------------------");
 foreach(var entry in namesList) {
     Console.WriteLine($"{entry.Key} is {entry.Value} years old");
 }
-Console.WriteLine("------------------------------");
+//* Console.WriteLine("------------------------------");
 
 foreach(var entry in namesList){
     int results = entry.Value;
@@ -55,13 +55,13 @@ foreach(var entry in namesList){
         Console.WriteLine("This is not a number");
     }
 }
-Console.WriteLine("------------------------------");
+//* Console.WriteLine("------------------------------");
 
 List<int> gradeList = new List<int>();
 gradeList.Add(99);
 gradeList.Add(80);
 gradeList.Add(95);
-Console.WriteLine("------------------------------");
+//* Console.WriteLine("------------------------------");
 
 static double average(List<int> list){
     int sum = 0;
@@ -71,7 +71,7 @@ static double average(List<int> list){
     return (double)sum/list.Count; // The double here allows for the sum (declared as int and Count default of int to return as a double)
 }
 Console.WriteLine(average(gradeList));
-Console.WriteLine("------------------------------");
+//* Console.WriteLine("------------------------------");
 // dictionary<string, int> namesList = new dictionary<string, int>();
 Random random = new Random();
 int rNum = random.Next(namesList.Count); // creating a var rKey making it = a random number no larger than the number of idexes in the dictionary
@@ -79,7 +79,7 @@ int rNum = random.Next(namesList.Count); // creating a var rKey making it = a ra
 KeyValuePair<string, int> aKey = namesList.ElementAt(rNum); // KeyValuePair is a predefined class.  This is where we are pulling the random entry from the dictionary and assigning aKey to it
 Console.WriteLine("key: " + aKey.Key + ", value: " + aKey.Value);
 
-Console.WriteLine("------------------------------");
+//* Console.WriteLine("------------------------------");
 // dictionary<string, int> namesList = new dictionary<string, int>();
 // namesList.Add("Lizette", 23);
 // namesList.Add("Melissa", 44);
@@ -88,7 +88,7 @@ foreach(var age in namesList) {
     Console.WriteLine("age: " + age.Value);
 }
 
-Console.WriteLine("------------------------------");
+//* Console.WriteLine("------------------------------");
 string name = "Melissa";
 string altName = "Kaitlynn";
 Console.WriteLine(String.Format("Hello, {0}, my name is {1}", name, altName));
