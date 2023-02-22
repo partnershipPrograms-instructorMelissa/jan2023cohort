ed8078853b62afdee9cd97880931f63fd4260a62
Emily Vazquez <emily.m.vazquez99@gmail.com>
2023-02-22 09:40:13 -0600

    finished fund 3 assignment

--- assignments/cSharp/day2/FundamentalsThree/Program.cs
+++ assignments/cSharp/day2/FundamentalsThree/Program.cs
@@ -45,18 +45,18 @@
 
 
 //* 4. Square the Values
-static List<int> SquareValues(List<int> IntList)
-{
-  for (int i = 0; i < IntList.Count; i++)
-  {
-    IntList[i] *= IntList[i];
-  }
-  return IntList;
-}
-List<int> TestIntList3 = new List<int>() { 1, 2, 3, 4, 5 };
-// You should get back [1,4,9,16,25], think about how you will show that this worked
-SquareValues(TestIntList3);
-TestIntList3.ForEach((n) => Console.WriteLine($"{TestIntList3[0]} squared is {n}"));
+// static List<int> SquareValues(List<int> IntList)
+// {
+//   for (int i = 0; i < IntList.Count; i++)
+//   {
+//     IntList[i] *= IntList[i];
+//   }
+//   return IntList;
+// }
+// List<int> TestIntList3 = new List<int>() { 1, 2, 3, 4, 5 };
+// // You should get back [1,4,9,16,25], think about how you will show that this worked
+// SquareValues(TestIntList3);
+// TestIntList3.ForEach((n) => Console.WriteLine($"Squared result is {n}"));
 
 
 
@@ -79,9 +79,76 @@ TestIntList3.ForEach((n) => Console.WriteLine($"{TestIntList3[0]} squared is {n}
 
 
 //* 6. Print Dictionary
+static void PrintDictionary(Dictionary<string, string> MyDictionary)
+{
+  foreach (var hero in MyDictionary)
+  {
+    // Console.WriteLine($"{hero.Key} is {hero.Value}");
+  }
+}
+Dictionary<string, string> TestDict = new Dictionary<string, string>();
+TestDict.Add("HeroName", "Iron Man");
+TestDict.Add("RealName", "Tony Stark");
+TestDict.Add("Powers", "Money and intelligence");
+PrintDictionary(TestDict);
 
 
 //* 7. Find Key 
+static bool FindKey(Dictionary<string, string> MyDictionary, string SearchTerm)
+{
+  bool exists = false;
+  foreach (var item in MyDictionary)
+  {
+    if (item.Key == SearchTerm)
+    {
+      exists = true;
+    }
+  }
+  return exists;
+}
+// Use the TestDict from the earlier example or make your own
+// This should print true
+Console.WriteLine(FindKey(TestDict, "RealName"));
+// This should print false
+Console.WriteLine(FindKey(TestDict, "Name"));
 
 
 //* 8. Generate a Dictionary 
+// Ex: Given ["Julie", "Harold", "James", "Monica"] and [6,12,7,10], return a dictionary
+// {
+//	"Julie": 6,
+//	"Harold": 12,
+//	"James": 7,
+//	"Monica": 10
+// } 
+static Dictionary<string, int> GenerateDictionary(List<string> Names, List<int> Numbers)
+{
+  Dictionary<string, int> dictionary = new Dictionary<string, int>();
+  for (int i = 0; i < Names.Count; i++)
+  {
+    dictionary.Add(Names[i], Numbers[i]);
+  }
+  return dictionary;
+}
+
+Console.WriteLine("--------------------------------------------------------");
+
+// We've shown several examples of how to set your tests up properly, it's your turn to set it up!
+// Your test code here
+List<string> name = new List<string>();
+name.Add("Julie");
+name.Add("Harold");
+name.Add("James");
+name.Add("Monica");
+
+List<int> number = new List<int>();
+number.Add(6);
+number.Add(12);
+number.Add(7);
+number.Add(10);
+
+Dictionary<string, int> result = GenerateDictionary(name, number);
+foreach (var entry in result)
+{
+  Console.WriteLine($"{entry.Key} is number {entry.Value}");
+}
