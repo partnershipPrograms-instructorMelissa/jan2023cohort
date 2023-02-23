77a03d4713d6cef76933990915ba74420c60fae5
Emily Vazquez <emily.m.vazquez99@gmail.com>
2023-02-22 18:07:35 -0600

    completed game dev 1

--- assignments/cSharp/day4/GameDev1/Enemy.cs
+++ assignments/cSharp/day4/GameDev1/Enemy.cs
@@ -1,1 +1,32 @@
-// 
+//* Enemy Class
+
+class Enemy
+{
+  public string Name;
+  private int Health;
+  public List<Attack> AttackList;
+
+  public Enemy(string name)
+  {
+    Name = name;
+    Health = 100;
+    AttackList = new List<Attack>();
+  }
+
+  public void RandomAttack()
+  {
+    Random rand = new Random();
+    int attack = rand.Next(AttackList.Count);
+    Console.WriteLine($"{Name} used {AttackList[attack].Name}! Watch out!");
+  }
+
+  public int GetHealth()
+  {
+    return Health;
+  }
+
+  public void NewAttack(Attack newAttack){
+    AttackList.Add(newAttack);
+    Console.WriteLine($"{Name} just learned {newAttack.Name}!");
+  }
+}
