7784e62bb64d575321879c2b23a90809b85b2f4c
mligon115 <mligon115@yahoo.com>
2023-02-23 14:37:23 -0600

    saving

--- assignments/cSharp/DojoSurvey/Controllers/SurveyController.cs
+++ assignments/cSharp/DojoSurvey/Controllers/SurveyController.cs
@@ -4,6 +4,7 @@ using DojoSurvey.Models;
 namespace DojoSurvey.Controllers;
 public class SurveyController : Controller
 {
+    
     [HttpGet("")]
     public IActionResult Index()
     {
@@ -13,6 +14,23 @@ public class SurveyController : Controller
     [HttpPost("survey")]
     public IActionResult Submission(Survey yourSurvey)
     {
-        return View("Result", yourSurvey);
+        if (ModelState.IsValid)
+        {
+            // Do somethng! Maybe insert into a database or log data to the console  
+            // Then we will redirect to a new page        
+            return RedirectToAction("Result", yourSurvey);
+        }
+        else
+        {
+            // Oh no! We need to return a ViewResponse to preserve the ModelState and the errors it now contains! 
+            // Make sure you return the View that contains the form!       
+            return View("Index");
+        }
     }
+
+    // [HttpPost("survey")]
+    // public IActionResult Submission(Survey yourSurvey)
+    // {
+    //     return View("Result", yourSurvey);
+    // }
 }
