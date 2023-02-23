d5d1d7e2b1971c52c9c55eb98fb81ec45d2c125f
mligon115 <mligon115@yahoo.com>
2023-02-23 11:02:06 -0600

    saving

--- assignments/cSharp/DojoSurvey/Controllers/SurveyController.cs
+++ assignments/cSharp/DojoSurvey/Controllers/SurveyController.cs
@@ -10,21 +10,9 @@ public class SurveyController : Controller
         return View();
     }
 
-    [HttpGet("survey")]
-    public IActionResult RegForm()
-    {
-        return View();
-    }
-
     [HttpPost("survey")]
     public IActionResult Submission(Survey yourSurvey)
     {
         return View("Result", yourSurvey);
     }
-
-    public IActionResult Result()
-    {
-        return View();
-    }
-
 }
