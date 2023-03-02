2f4037a85dc4313e1dde825405b29332181c31cb
Kaitlynn Whitney <kailiwhitney@hotmail.com>
2023-03-02 08:58:16 -0600

    notes for week 3

--- notes/cSharp/w3/CRApractice/Controllers/SquishyController.cs
+++ notes/cSharp/w3/CRApractice/Controllers/SquishyController.cs
@@ -18,6 +18,7 @@ public class SquishyController : Controller
         return View("AllSquishies", allSquishies);
     }
 
+    // create
     [HttpGet("addSquishy")]
     public IActionResult AddSquishy(){
         return View();
@@ -35,32 +36,58 @@ public class SquishyController : Controller
         return View("addSquishy");
     }
 
-    // [HttpPost("monsters/{MonsterId}/update")]
-    // public IActionResult UpdateMonster(Monster newMon, int MonsterId)
-    // {
-    //     // 2. Find the old version of the instance in your database
-    //     Monster? OldMonster = _context.Monsters.FirstOrDefault(i => i.MonsterId == MonsterId);
-    //     // 3. Verify that the new instance passes validations
-    //     if(ModelState.IsValid)
-    //     {
-    //         // 4. Overwrite the old version with the new version
-    //         // Yes, this has to be done one attribute at a time
-    //         OldMonster.Name = newMon.Name;
-    //         OldMonster.Height = newMon.Height;
-    //         OldMonster.Description = newMon.Description;
-    //         // You updated it, so update the UpdatedAt field!
-    //         OldMonster.UpdatedAt = DateTime.Now;
-    //         // 5. Save your changes
-    //         _context.SaveChanges();
-    //         // 6. Redirect to an appropriate page
-    //         return RedirectToAction("Index");
-    //     } else {
-    //         // 3.5. If it does not pass validations, show error messages
-    //         // Be sure to pass the form back in so you don't lose your changes
-    //         // It should be the old version so we can keep the ID
-    //         return View("EditMonster", OldMonster);
-    //     }
-    // }
+    // read - view one
+    [HttpGet("squishy/{squishyID}/view")]
+    public IActionResult ViewSquishy(int squishyID){
+        Squishy? item = _context.Squishies.FirstOrDefault(i => i.ID == squishyID);
+        if(item == null){
+            return RedirectToAction("AllSquishies");
+        }
+        return View(item);
+    }
+
+    // update
+    [HttpGet("squishy/{squishyID}/edit")]
+    public IActionResult EditSquishy(int squishyID){
+        Squishy? item = _context.Squishies.FirstOrDefault(i => i.ID == squishyID);
+        if(item == null){
+            return RedirectToAction("AllSquishies");
+        }
+        else{
+            return View(item);
+        }
+        
+    }
+
+    [HttpPost("squishy/{squishyID}/update")]
+    public IActionResult UpdateSquishy(Squishy newItem, int squishyID){
+        Squishy? oldItem = _context.Squishies.FirstOrDefault(i => i.ID == squishyID);
+        if(ModelState.IsValid){
+            // update info
+            oldItem.Name = newItem.Name;
+            oldItem.Img = newItem.Img;
+            oldItem.Location = newItem.Location;
+            oldItem.UpdatedAt = DateTime.Now;
+            // save new info
+            _context.SaveChanges();
+            return RedirectToAction("AllSquishies");
+        }
+        else{
+            return View("EditSquishy", squishyID);
+        }
+    }
+
+    // delete
+    [HttpGet("squishy/{squishyID}/delete")]
+    public IActionResult DeleteSquishy(int squishyID){
+        Squishy? itemToDelete = _context.Squishies.SingleOrDefault(i => i.ID == squishyID);
+        if(itemToDelete != null){
+            _context.Squishies.Remove(itemToDelete);
+            _context.SaveChanges();
+        }
+        return RedirectToAction("AllSquishies");
+
+    }
 
     // [HttpPost("monsters/{MonsterId}/destroy")]
     // public IActionResult DestroyMonster(int MonsterId)
